676744cd74c34845302612024970fb595ad12e80
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2019-12-13 11:23:23 +0100

    first commit

---
+++ Spice/Data/Migrations/20191115150654_CategoryNameTypeChange.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Data.Migrations
+{
+    public partial class CategoryNameTypeChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
