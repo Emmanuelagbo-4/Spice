a241bac907e1f97b9308df28f53669eb3c78182c
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2019-12-17 14:34:49 +0100

    Created Post method for Edit and Addded Delete and Details Page

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -60,5 +60,61 @@ namespace Spice.Areas.Admin.Controllers
             }
             return View(category);
         }
+
+        //POST --EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        //GET --DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //POST --DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Category category)
+        {
+            _db.Remove(category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
     }
 }
--- Spice/Models/Category.cs
+++ Spice/Models/Category.cs
@@ -11,6 +11,8 @@ namespace Spice.Models
         [Key]
         public int Id { get; set; }
         [Required]
+
+        [Display(Name="Category Name")]
         public string Name { get; set; }
     }
 }
