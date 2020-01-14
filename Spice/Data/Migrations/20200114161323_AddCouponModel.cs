48506de7d25ecf0df8b7eace4d3e14dec2563673
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2020-01-14 17:25:00 +0100

    Add Coupon Model

---
+++ Spice/Data/Migrations/20200114161323_AddCouponModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Data.Migrations
+{
+    public partial class AddCouponModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "MenuItem",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Coupon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CouponType = table.Column<string>(nullable: false),
+                    Discount = table.Column<double>(nullable: false),
+                    MinimumAmount = table.Column<double>(nullable: false),
+                    Picture = table.Column<byte[]>(nullable: true),
+                    isActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupon", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupon");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "MenuItem",
+                newName: "id");
+        }
+    }
+}
