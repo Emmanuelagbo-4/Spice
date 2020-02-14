6c3d4eb1d1c951007bbb96e019dc3af28615039c
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2020-02-14 18:26:20 +0100

    OnPostAsync For Register and Added new Roles

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -30,12 +30,14 @@ namespace Spice
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
-
+        //options => options.SignIn.RequireConfirmedAccount = true
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
