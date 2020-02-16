cf919adae1c6ddb921d7e9ee9d7cd859f360cf0d
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2020-02-16 21:51:31 +0100

    Lock and UnLock User

--- Spice/Areas/Admin/Controllers/UserController.cs
+++ Spice/Areas/Admin/Controllers/UserController.cs
@@ -25,5 +25,39 @@ namespace Spice.Areas.Admin.Controllers
 
             return View(await _db.ApplicationUser.Where(u=>u.Id != claim.Value).ToListAsync());
         }
+
+        public async Task<IActionResult> Lock(string Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(k => k.Id == Id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            applicationUser.LockoutEnd = DateTime.Now.AddYears(1000);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UnLock(string Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(k => k.Id == Id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            applicationUser.LockoutEnd = DateTime.Now;
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
